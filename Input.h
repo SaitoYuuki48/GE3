396ec6673c2423cef2e89eb5632cd8f2ca455c70
SaitoYuuki48 <115055003+SaitoYuuki48@users.noreply.github.com>
2023-10-10 15:28:53 +0900

    02_02

--- Input.h
+++ Input.h
@@ -1,6 +1,13 @@
 ﻿#pragma once
 #include <Windows.h>
 
+#define DIRECTINPUT_VERSION     0x0800   // DirectInputのバージョン指定
+#include <dinput.h>
+#include <wrl.h>
+
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
+
 class Input
 {
 public: //メンバ関数
@@ -10,5 +17,13 @@ public: //メンバ関数
 	//更新
 	void Update();
 
+public:
+	//namespace省略
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+private: //メンバ変数
+	// キーボードのデバイス
+	ComPtr<IDirectInputDevice8> keyboard;
+	//Microsoft::WRL::ComPtr<IDirectInputDevice8> keyboard;
 };
 
