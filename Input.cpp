921dd0f6f368f149c64015dea0ba34ba8401f65d
SaitoYuuki48 <115055003+SaitoYuuki48@users.noreply.github.com>
2023-10-17 14:55:35 +0900

    02_03

--- Input.cpp
+++ Input.cpp
@@ -27,10 +27,35 @@ void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
 
 void Input::Update()
 {
-    BYTE key[256] = {};
+
+    //前回のキー入力を保存
+    memcpy(keyPre, key, sizeof(key));
+    // ↑ keyPreの中にkeyの情報をコピーする
+    /*for (int i = 0; i < 256; i++) {
+        keyPre[i] = key[i];
+    }*/
 
     // キーボード情報の取得開始
     keyboard->Acquire();
     // 全キーの入力状態を取得する
     keyboard->GetDeviceState(sizeof(key), key);
+}
+
+bool Input::PushKey(BYTE keyNumber)
+{
+    //指定キーを押していればtrueを返す
+    if (key[keyNumber]) {
+        return true;
+    }
+    //そうでなければfalseを返す
+    return false;
+}
+
+bool Input::TriggerKey(BYTE keyNumber)
+{
+    if (key[keyNumber] && keyPre[keyNumber] == 0) {
+        return true;
+    }
+
+    return false;
 }
--- Input.h
+++ Input.h
@@ -21,9 +21,28 @@ public:
 	//namespace省略
 	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
 
+	/// <summary>
+	/// キーの押下をチェック
+	/// </summary>
+	/// <param name="keyNumber">キー番号( DIK_0 等)</param>
+	/// <returns>押されているか</returns>
+	bool PushKey(BYTE keyNumber);
+
+	/// <summary>
+	/// キーのトリガーをチェック
+	/// </summary>
+	/// <param name="keyNumber">キー番号( DIK_0 等)</param>
+	/// <returns>トリガーか</returns>
+	bool TriggerKey(BYTE keyNumber);
+
 private: //メンバ変数
 	// キーボードのデバイス
 	ComPtr<IDirectInputDevice8> keyboard;
 	//Microsoft::WRL::ComPtr<IDirectInputDevice8> keyboard;
+
+	 //全キーの状態
+	BYTE key[256] = {};
+	//前回の全キーの状態
+	BYTE keyPre[256] = {};
 };
 
