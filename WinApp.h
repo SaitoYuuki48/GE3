2a7fd8636eef538a463074b777ae349a50b379fe
SaitoYuuki48 <115055003+SaitoYuuki48@users.noreply.github.com>
2023-10-24 14:33:16 +0900

    03_01

--- WinApp.h
+++ WinApp.h
@@ -1,7 +1,12 @@
 #pragma once
+#include <Windows.h>
+
 class WinApp
 {
 public: // メンバ関数
+	// ウィンドウプロシージャ
+	LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
 	// 初期化
 	void Initialize();
 	// 更新
