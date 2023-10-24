2c50bb740aa2e5f7aabbdbcd62969e574faa7886
SaitoYuuki48 <115055003+SaitoYuuki48@users.noreply.github.com>
2023-10-24 14:45:43 +0900

    03_02

--- WinApp.h
+++ WinApp.h
@@ -3,13 +3,13 @@
 
 class WinApp
 {
-public: // メンバ関数
+public: //静的メンバ関数
 	// ウィンドウプロシージャ
-	LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+	static LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
+public: // メンバ関数
 	// 初期化
 	void Initialize();
 	// 更新
 	void Update();
 };
-
