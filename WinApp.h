0d97d999556587c948518055c4dc6b72c39aff70
SaitoYuuki48 <115055003+SaitoYuuki48@users.noreply.github.com>
2023-10-24 15:15:35 +0900

    03_03

--- WinApp.h
+++ WinApp.h
@@ -12,4 +12,17 @@ public: // メンバ関数
 	void Initialize();
 	// 更新
 	void Update();
+
+	//Getter
+	HWND GetHwnd() const { return hwnd; }
+	HINSTANCE GetHInstance()const { return w.hInstance; }
+
+public:
+	// ウィンドウサイズ
+	static const int window_width = 1280;  // 横幅
+	static const int window_height = 720;  // 縦幅
+
+private:
+	HWND hwnd;
+	WNDCLASSEX w{};
 };
